a499f560ddb1fb4f4df1de133364b82cedc19668
Tamara Sancristobal <tamarasancristobal@gmail.com>
2024-06-11 12:53:58 -0300

    Added new product images

--- assets/js/system.js
+++ assets/js/system.js
@@ -202,7 +202,7 @@ class System {
                 if (this.allPurchases[index].purchaseStatus === status) {
                     allStatesList += `
                         <li class="${this.allPurchases[index].purchaseStatus}">
-                            <img src="./assets/img/products/product-tshirt.png" class="left">
+                            <img src="./assets/img/products/${this.allPurchases[index].product.productImg}.png" class="left">
                             <div class="right">
                                 <div class="top-wrapper">
                                     <p>${this.allPurchases[index].purchaseStatus} order</p>
@@ -212,7 +212,7 @@ class System {
                                     ${this.allPurchases[index].product.productName}
                                 </p>
                                 <div class="middle-wrapper">
-                                    <span class="description">Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior</span>
+                                    <span class="description">${this.allPurchases[index].product.productDescription}</span>
                                     <div class="middle-right">
                                         <p><span>$${this.allPurchases[index].product.productPrice}</span> x${this.allPurchases[index].quantity}</p>
                                     </div>
@@ -229,7 +229,7 @@ class System {
                 if (this.allPurchases[index].purchaseStatus === status && this.allPurchases[index].buyerID === currentUser.id) {
                     allStatesList += `
                         <li class="${this.allPurchases[index].purchaseStatus}">
-                            <img src="./assets/img/products/product-tshirt.png" class="left">
+                            <img src="./assets/img/products/${this.allPurchases[index].product.productImg}.png" class="left">
                             <div class="right">
                                 <div class="top-wrapper">
                                     <p>${this.allPurchases[index].purchaseStatus} order</p>
@@ -239,7 +239,7 @@ class System {
                                     ${this.allPurchases[index].product.productName}
                                 </p>
                                 <div class="middle-wrapper">
-                                    <span class="description">Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior Loreim impsum dolior</span>
+                                    <span class="description">${this.allPurchases[index].product.productDescription}</span>
                                     <div class="middle-right">
                                         <p><span>$${this.allPurchases[index].product.productPrice}</span> x${this.allPurchases[index].quantity}</p>
                                     </div>
@@ -271,15 +271,16 @@ class System {
         this.addAdmin(new Admin("bob_admin", "BobAdmin123"));
         // Products
         this.addProduct(new Product("T-shirt Violet", 500, "Centered violet print with Korn logo", "product-tshirt", 10, true, false));
-        this.addProduct(new Product("T-shirt Golden Years", 600, "Centered gold print with vintage Adidas logo", "product-tshirt", 5, false, false));
-        this.addProduct(new Product("Hoodie Korn", 800, "Black hoodie with white centered Korn logo", "product-tshirt", 0, true, true));
-        this.addProduct(new Product("Cargo Black Pants", 1500, "Black long cargo pants with side pockets", "product-tshirt", 10, true, true));
-        this.addProduct(new Product("Scottish Miniskirt", 1200, "Miniskirt with black and white scottish print", "product-tshirt", 5, true, false));
-        this.addProduct(new Product("Jean Jacket", 2500, "Classic black jean jacket", "product-tshirt", 5, true, true));
-        this.addProduct(new Product("Denim Jeans", 800, "Blue denim jeans with a slim fit", "product-tshirt", 10, true, false));
-        this.addProduct(new Product("Sneakers Classic", 1200, "White sneakers with a classic design", "product-tshirt", 15, true, false));
-        this.addProduct(new Product("Korn Socks", 100, "White socks with Korn print", "product-tshirt", 20, true, false));
-        this.addProduct(new Product("Summer Dress", 500, "Light and airy summer dress with Korn print", "product-tshirt", 10, true, false));
+        this.addProduct(new Product("T-shirt Golden Years", 600, "Centered gold print with vintage Adidas logo", "product-golden-tshirt", 5, false, false));
+        this.addProduct(new Product("Hoodie Korn", 1100, "Black hoodie with white centered Korn logo", "product-hoodie", 0, true, true));
+        this.addProduct(new Product("Black Sport Pants", 1500, "Black sport pants with side pockets", "product-black-pants", 10, true, true));
+        this.addProduct(new Product("Scottish Miniskirt", 600, "Miniskirt with black and white scottish print", "product-skirt", 5, true, false));
+        this.addProduct(new Product("Long Sleeve", 800, "Long sleeve with front print", "product-long-sleeve", 5, true, true));
+        this.addProduct(new Product("Sport Jacket", 1200, "Black sport jacket with a slim fit", "product-sport-jacket", 10, true, false));
+        this.addProduct(new Product("Sneakers Classic White", 1200, "White sneakers with a classic design", "product-white-sneakers", 15, true, false));
+        this.addProduct(new Product("Green Korn Socks", 100, "Green socks with Korn print", "product-green-socks", 20, true, false));
+        this.addProduct(new Product("Violet Jacket", 1300, "Black sport jacket with a regular fit", "product-violet-jacket", 10, true, false));
+        this.addProduct(new Product("Black Sunglasses", 200, "Regular black sunglasses ", "product-sunglasses", 10, true, false));
     }
 
 }
